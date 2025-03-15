5779419051038119ded9e73164f36bdac769d3f3
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2025-03-15 15:56:30 +0800

    Add files via upload

---
+++ Automation and Scripting with Python/main.py
@@ -0,0 +1,22 @@
+# Step 3.1: Fetch HTML Content
+### YOUR CODE HERE ###
+
+# Step 3.2: Extract the Required Data
+### YOUR CODE HERE ###
+
+# Step 4.1: Convert to a DataFrame
+# Import pandas
+### YOUR CODE HERE ###
+
+# Convert the game data into a pandas DataFrame
+### YOUR CODE HERE ###
+
+# Inspect the DataFrame
+### YOUR CODE HERE ###
+
+# Save and print the shaped data
+### YOUR CODE HERE ###
+
+# Step 5.1: Save to a CSV File
+# Save the DataFrame to a CSV file named sports_statistics.csv
+### YOUR CODE HERE ###
